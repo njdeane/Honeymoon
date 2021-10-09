850c99cc573cc0a374f00e45dcb2378ff03409f2
Nic Deane <njdeane7@gmail.com>
2021-10-09 12:19:37 +1000

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Nic Deane on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
